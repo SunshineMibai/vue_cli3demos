e244bbe04ca2d51ee07f688db034ebfa3ad086cb
jiaoguoliang <jiaoguoliang1990@163.com>
2019-03-11 00:18:49 +0800

    vue_cli3_demos2

---
+++ vue_demos_2/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<div>
+			<p v-text='test'></p>
+			<p v-html='test'></p>
+		</div>
+		<input type="text" v-model='inputValue'>
+		<input type="checkbox" v-model='inputChecked'>
+		<ul>
+			<li v-for='(item,index) in dataList'>内容是：{{item}}---下标是：{{index}}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+	name: 'home',
+	components: {
+		HelloWorld
+	},
+	data() {
+		return {
+			test:'<p>this is p</p>',
+			inputValue:'123',
+			inputChecked:true,
+			dataList:[1,2,3]
+		}
+	}
+}
+</script>
